e88264a0c1197bdd87369a6d2b3bab0e51203cdc
Mohit Ahuja <ahujamoh@gmail.com>
2018-07-22 16:31:02 +0200

    setting widget variables

--- app/src/main/java/concentration/tmc/ahujamoh/com/tmc_concentratie/Activities/GameActivity.java
+++ app/src/main/java/concentration/tmc/ahujamoh/com/tmc_concentratie/Activities/GameActivity.java
@@ -1,31 +1,51 @@
 package concentration.tmc.ahujamoh.com.tmc_concentratie.Activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Chronometer;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import concentration.tmc.ahujamoh.com.tmc_concentratie.R;
 
 public class GameActivity extends AppCompatActivity {
+    public static final String TAG = GameActivity.class.getSimpleName();
+
+    private RecyclerView mRecyclerView;
+    private TextView mScoreTextView;
+    private Chronometer mChronometer;
+    private ProgressBar mProgressBar;
+    private ConstraintLayout mConstraintLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        setUpWidgetVariables();
+
+        getImages();
+
     }
 
+    private void setUpWidgetVariables() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView); //TODO: set up adapter
+        mScoreTextView = (TextView) findViewById(R.id.score_textview);
+        mChronometer = (Chronometer) findViewById(R.id.chronometer);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mConstraintLayout = (ConstraintLayout) findViewById(R.id.gameConstraintLayout);
+    }
+
+    private void getImages(){
+        Log.v(TAG, "call retrofit to fetch images here");
+    }
 }
