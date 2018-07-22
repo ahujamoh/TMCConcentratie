0fd568fbf13d46e38e6d9038c8217aaa4b2b2602
Mohit Ahuja <ahujamoh@gmail.com>
2018-07-22 17:51:43 +0200

    changes for retrofit client to create call

--- app/src/main/java/concentration/tmc/ahujamoh/com/tmc_concentratie/ImageDownloader/FlickrAPIEndpoints.java
+++ app/src/main/java/concentration/tmc/ahujamoh/com/tmc_concentratie/ImageDownloader/FlickrAPIEndpoints.java
@@ -1,4 +1,14 @@
 package concentration.tmc.ahujamoh.com.tmc_concentratie.ImageDownloader;
 
-public class FlickrAPIEndpoints {
+import concentration.tmc.ahujamoh.com.tmc_concentratie.Models.Gallery;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface FlickrAPIEndpoints {
+
+    @GET("?method=flickr.photos.search")
+    Call<Gallery> getPhotosForCats(@Query("text") String query,
+                                    @Query("per_page") int perPage,
+                                    @Query("page") int page);
 }
--- app/src/main/java/concentration/tmc/ahujamoh/com/tmc_concentratie/ImageDownloader/RetrofitFlickrAPIClient.java
+++ app/src/main/java/concentration/tmc/ahujamoh/com/tmc_concentratie/ImageDownloader/RetrofitFlickrAPIClient.java
@@ -1,11 +1,56 @@
 package concentration.tmc.ahujamoh.com.tmc_concentratie.ImageDownloader;
 
-public class RetrofitFlickrAPIObject {
-    private static final String TAG = RetrofitFlickrAPIObject.class.getSimpleName();
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitFlickrAPIClient {
+    private static final String TAG = RetrofitFlickrAPIClient.class.getSimpleName();
     private static String API_KEY = "APIKEY";
     public static final String BASE_URL = "https://api.flickr.com/services/rest/";
     // https://api.flickr.com/services/rest/?method=flickr.photos.search
-    // &api_key=<APIKEY>&text=cat&extras=url_s&format=json&nojsoncallback=1
 
+    private static Retrofit retrofit = null;
+
+    public static <T> T call(Class<T> serviceClass) {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+
+
+        httpClientBuilder.addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request();
+                HttpUrl httpUrl = request.url().newBuilder()
+                        // &api_key=<APIKEY>&extras=url_s&format=json&nojsoncallback=1
+                        .addQueryParameter("api_key", API_KEY)
+                        .addQueryParameter("format", "json")
+                        .addQueryParameter("nojsoncallback", "1")
+                        .addQueryParameter("extras", "url_s")
+                        .build();
+                request = request.newBuilder().url(httpUrl).build();
+                return chain.proceed(request);
+            }
+        });
+
+        httpClientBuilder.addInterceptor(interceptor);
+        OkHttpClient client = httpClientBuilder.build();
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create()).client(client).build();
+
+        return retrofit.create(serviceClass);
+    }
 
 }
