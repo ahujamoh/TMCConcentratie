3cc188e39ce57ea4aff041ecf6219ca2cec0c3d8
Mohit Ahuja <ahujamoh@gmail.com>
2018-07-22 17:50:22 +0200

    adding interceptor

---
+++ app/src/main/java/concentration/tmc/ahujamoh/com/tmc_concentratie/ImageDownloader/FlickrAPIEndpoints.java
@@ -0,0 +1,4 @@
+package concentration.tmc.ahujamoh.com.tmc_concentratie.ImageDownloader;
+
+public class FlickrAPIEndpoints {
+}
---
+++ app/src/main/java/concentration/tmc/ahujamoh/com/tmc_concentratie/ImageDownloader/RetrofitFlickrAPIClient.java
@@ -0,0 +1,11 @@
+package concentration.tmc.ahujamoh.com.tmc_concentratie.ImageDownloader;
+
+public class RetrofitFlickrAPIObject {
+    private static final String TAG = RetrofitFlickrAPIObject.class.getSimpleName();
+    private static String API_KEY = "APIKEY";
+    public static final String BASE_URL = "https://api.flickr.com/services/rest/";
+    // https://api.flickr.com/services/rest/?method=flickr.photos.search
+    // &api_key=<APIKEY>&text=cat&extras=url_s&format=json&nojsoncallback=1
+
+
+}
